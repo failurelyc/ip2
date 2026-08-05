4a2c706b12219bc154d587a8e283cc1f97766890
Li Yi Cheng <liyicheng2003@gmail.com>
2026-08-05 23:42:41 +0800

    Add echo

--- src/main/java/Nova.java
+++ src/main/java/Nova.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
+
+/**
+ * Runs Nova's command-line conversation.
+ */
 public class Nova {
+    private static final String SEPARATOR = "____________________________________________________________";
+
+    /**
+     * Starts Nova, echoes each command, and exits when {@code bye} is entered.
+     *
+     * @param args command-line arguments, which are not used
+     */
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
+        System.out.println(SEPARATOR);
         String banner = " _   _  _____   ____   \n"
                 + "| \\ | || ____| / ___|  \n"
                 + "|  \\| ||  _|   \\___ \\  \n"
@@ -8,8 +20,21 @@ public class Nova {
                 + "|_| \\_||_____| |____/  \n";
         System.out.print(banner);
         System.out.println("Hello! I'm Nova.\nWhat can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        System.out.println(SEPARATOR);
+
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String command = scanner.nextLine();
+            System.out.println(SEPARATOR);
+
+            if (command.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(SEPARATOR);
+                break;
+            }
+
+            System.out.println(" " + command);
+            System.out.println(SEPARATOR);
+        }
     }
 }
