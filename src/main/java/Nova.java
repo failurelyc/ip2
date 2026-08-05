d5a1bcf011101c2aae9b14f8169a0575de289092
Li Yi Cheng <liyicheng2003@gmail.com>
2026-08-05 23:35:34 +0800

    Add greetings

---
+++ src/main/java/Nova.java
@@ -0,0 +1,15 @@
+public class Nova {
+    public static void main(String[] args) {
+        System.out.println("____________________________________________________________");
+        String banner = " _   _  _____   ____   \n"
+                + "| \\ | || ____| / ___|  \n"
+                + "|  \\| ||  _|   \\___ \\  \n"
+                + "| |\\  || |___   ___) | \n"
+                + "|_| \\_||_____| |____/  \n";
+        System.out.print(banner);
+        System.out.println("Hello! I'm Nova.\nWhat can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
